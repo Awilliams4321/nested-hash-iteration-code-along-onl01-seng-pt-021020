451eef68e94cf5ed50fe71bc27714c419324d62f
Amber Williams <awilliams1234@live.com>
2020-02-23 02:10:51 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,6 @@ require 'pry'
       contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
     end
   end
 end
